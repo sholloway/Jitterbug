7ddd987569ac421027d77591317a9b91096ecdff
Samuel Holloway <sholloway@gmail.com>
2012-01-22 10:12:13 -0600

    updated README with build info, removed unnessacry rake tasks

--- build_scripts/ruby_test_runner.rb
+++ build_scripts/ruby_test_runner.rb
@@ -8,12 +8,3 @@ RSpec::Core::RakeTask.new(:run_ruby_tests) do |t|
 	t.pattern = "tests/**/*_spec.rb"
 	t.ruby_opts = ruby_options 	
 end
-
-desc "Run all ruby tests using rspec"
-RSpec::Core::RakeTask.new(:test) do |t|  
-	ruby_options = "-I\"#{File.join(File.expand_path(File.dirname(__FILE__)),"../vendor")}\" -I\"#{File.join(File.expand_path(File.dirname(__FILE__)),"../lib")}\""
-  t.rspec_opts = %w[--color --format d]
-  t.verbose = true
-	t.pattern = "tests/**/layers_validation_spec.rb"
-	t.ruby_opts = ruby_options 	
-end
